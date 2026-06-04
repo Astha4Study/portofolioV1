c0d25b0d18831f503a8476c3e4113d4288ff633b
Astha <iyandabes1@gmail.com>
2026-06-04 09:46:25 +0700

    fix: Command prisma generate --schema=./server/prisma/schema.prisma && turbo build --filter=shared --filter=server exited with 1

--- server/src/index.ts
+++ server/src/index.ts
@@ -222,10 +222,3 @@ app.get("/wakatime/stats", async (c) => {
 
 export default app;
 
-// For Vercel serverless
-export const GET = app.fetch;
-export const POST = app.fetch;
-export const PUT = app.fetch;
-export const DELETE = app.fetch;
-export const PATCH = app.fetch;
-export const OPTIONS = app.fetch;
